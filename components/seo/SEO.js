a13c5d8ae544ab234ac13343d34b430ec7964c64
adamovic6666 <adamovicslobodan@yahoo.com>
2022-11-15 16:43:53 +0100

    added seo

--- components/seo/SEO.js
+++ components/seo/SEO.js
@@ -1,70 +1,37 @@
-import Head from "next/head";
-
+import { NextSeo } from "next-seo";
 const SEO = ({ metaTitle }) => {
   return (
-    <Head>
-      <title>{metaTitle ? metaTitle : "Title"}</title>
-      <meta name="description" content="descriptoin-placeholder" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#ffffff" />
-      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/apple-icon-114x114.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/apple-icon-120x120.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="/apple-icon-144x144.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/apple-icon-152x152.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="msapplication-TileColor" content="#ffffff/" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff"></meta>
-    </Head>
+    <NextSeo
+      title={metaTitle}
+      description="This is a demo description"
+      // canonical="https://www.example.com"
+      openGraph={{
+        url: "https://replastic.vercel.app/",
+        title: "Open Graph Title",
+        description: "Open Graph Description",
+        images: [
+          {
+            // url: "https://www.example.com/og-image01.jpg",
+            url: "https://replastic.vercel.app/about-img.png",
+            width: 800,
+            height: 600,
+            alt: "Og Image Alt",
+            type: "image/png",
+          },
+          {
+            // url: "https://www.example.com/og-image02.jpg",
+            url: "https://replastic.vercel.app/about-img.png",
+            width: 900,
+            height: 800,
+            alt: "Og Image Alt Second",
+            type: "image/png",
+          },
+          { url: "https://www.example.com/og-image03.jpg" },
+          { url: "https://www.example.com/og-image04.jpg" },
+        ],
+        site_name: "https://replastic.vercel.app/",
+      }}
+    />
   );
 };
 
