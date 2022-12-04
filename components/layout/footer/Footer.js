6f42398edf559b0156efd578016679564bf27bb0
Slobodan Adamovic <slobodan@studiopresent.com>
2022-12-04 12:48:40 +0100

    changed footer and orientation lock

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={classes.Footer}>
       <div className="container">
-        <p>Replastic d.o.o. 2022 © Sva prava zadržana.</p>
+        <p>REPLASTIC D.O.O. 2022 © SVA PRAVA ZADRŽANA. </p>
         <Link href="/politika-privatnosti">Politika privatnosti</Link>
       </div>
     </footer>
